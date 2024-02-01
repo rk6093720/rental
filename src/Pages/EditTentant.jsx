009bbf8b7ce09fe80fd8296bef74127cac961b62
Rohit Kumar <97551067+rk6093720@users.noreply.github.com>
2024-02-01 23:33:56 +0530

    1feb

--- src/Pages/EditTentant.jsx
+++ src/Pages/EditTentant.jsx
@@ -5,7 +5,7 @@ import {
   FormControl,
   Input,
   Stack,
-  Checkbox
+  Checkbox,
 } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { editTentants, getTentants } from "../Redux/Tentants/action";
@@ -16,34 +16,55 @@ const EditTentant = () => {
   const { id } = useParams();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [email,setEmail]=useState("");
-  const [phone,setPhone]= useState("");
-  const [dateOfBirth,setDateOfBirth]= useState("");
-  const [aadharCard,setAadharCard]=useState("");
-  const [panCard,setPanCard]=useState("");
-   const [residentialAddress, setResidentialAddress] = useState({
-     street: "",
-     city: "",
-     state: "",
-     country: "",
-     zipCode: "",
-   });
-    const [currentAddress, setCurrentAddress] = useState({
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [aadharCard, setAadharCard] = useState("");
+  const [panCard, setPanCard] = useState("");
+  const [residentialAddress, setResidentialAddress] = useState({
+    street: "",
+    city: "",
+    state: "",
+    country: "",
+    zipCode: "",
+  });
+  const [currentAddress, setCurrentAddress] = useState({
+    street: "",
+    city: "",
+    state: "",
+    country: "",
+    zipCode: "",
+  });
+  const [registrationofRoomRent, setRegistrationofRoomRent] = useState("");
+  const [currentTentant, setCurrentTentant] = useState({});
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const tentant = useSelector((state) => state.Tentants.tentants);
+  const handleCheckbox = (e) => {
+    const { checked } = e.target;
+    if (checked) {
+      setCurrentAddress({
+        ...currentAddress,
+        street: residentialAddress.street,
+        city: residentialAddress.city,
+        state: residentialAddress.state,
+        country: residentialAddress.country,
+        zipCode: residentialAddress.zipCode,
+      });
+    } else {
+      setCurrentAddress({
+        ...currentAddress,
         street: "",
         city: "",
         state: "",
         country: "",
         zipCode: "",
       });
-    const [registrationofRoomRent,setRegistrationofRoomRent]=useState("");
-    const [currentTentant, setCurrentTentant] = useState({});
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const tentant = useSelector((state) => state.Tentants.tentants);
+    }
+  };
+
   const handleEditTentant = () => {
-    const payload = {
-   
-    };
+    const payload = {};
     dispatch(editTentants(id, payload))
       .then(() => dispatch(getTentants()))
       .then((r) => {
@@ -58,29 +79,70 @@ const EditTentant = () => {
   useEffect(() => {
     if (id) {
       const tentantById = tentant.find((lands) => lands._id === id);
+      console.log(tentantById);
       tentantById && setCurrentTentant(tentantById);
+      tentantById && setFirstName(tentantById.firstName);
+      tentantById && setLastName(tentantById.lastName);
+      tentantById && setEmail(tentantById.email);
+      tentantById && setPhone(tentantById.phone);
+      tentantById && setDateOfBirth(tentantById.dateOfBirth);
+      tentantById && setAadharCard(tentantById.aadharCard);
+      tentantById && setPanCard(tentantById.panCard);
+      tentantById && setCurrentAddress(tentantById?.currentAddress?.street);
+      tentantById && setCurrentAddress(tentantById?.currentAddress?.city);
+      tentantById && setCurrentAddress(tentantById?.currentAddress?.state);
+      tentantById && setCurrentAddress(tentantById?.currentAddress?.country);
+      tentantById && setCurrentAddress(tentantById?.currentAddress?.zipCode);
+      tentantById &&
+        setResidentialAddress(tentantById?.residentialAddress?.street);
+      tentantById &&
+        setResidentialAddress(tentantById?.residentialAddress?.city);
+      tentantById &&
+        setResidentialAddress(tentantById?.residentialAddress?.state);
+      tentantById &&
+        setResidentialAddress(tentantById?.residentialAddress?.country);
+      tentantById &&
+        setResidentialAddress(tentantById?.residentialAddress?.zipCode);
     }
   }, [id, tentant]);
   console.log(tentant);
   return (
     <div>
       <Stack style={{ width: "100%", height: "100vh", marginTop: "15px" }}>
-        <form id="my-form" novalidate="novalidate">
+        <form onSubmit={handleEditTentant}>
           <Box>
             <Flex>
               <FormControl>
                 <FormLabel>Firstname</FormLabel>
-                <Input id="firstName" type="text" placeholder="Firstname" />
+                <Input
+                  id="firstName"
+                  type="text"
+                  placeholder="Firstname"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel for="Lastname">LastName</FormLabel>
-                <Input id="lastName" type="text" placeholder="Lastname" />
+                <Input
+                  id="lastName"
+                  type="text"
+                  placeholder="Lastname"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
               </FormControl>
             </Flex>
             <Flex>
               <FormControl>
                 <FormLabel for="email">Email</FormLabel>
-                <Input id="email" type="email" placeholder="email" />
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel for="phone">Phone</FormLabel>
@@ -89,6 +151,8 @@ const EditTentant = () => {
                   type="text"
                   placeholder="phone"
                   maxlength="10"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
                 />
               </FormControl>
             </Flex>
@@ -101,6 +165,7 @@ const EditTentant = () => {
                   name="aadharCard"
                   accept="application/pdf"
                   placeholder="AadharCard"
+                  onChange={(e) => setAadharCard(e.target.files[0])}
                 />
               </FormControl>
               <FormControl>
@@ -111,13 +176,20 @@ const EditTentant = () => {
                   name="panCard"
                   accept="application/pdf"
                   placeholder="panCard"
+                  onChange={(e) => setPanCard(e.target.files[0])}
                 />
               </FormControl>
             </Flex>
             <Flex>
               <FormControl>
                 <FormLabel for="Date-of-birth">Date-of-birth</FormLabel>
-                <Input id="dateOfBirth" type="date" placeholder="dob" />
+                <Input
+                  id="dateOfBirth"
+                  type="date"
+                  placeholder="dob"
+                  value={dateOfBirth}
+                  onChange={(e) => setDateOfBirth(e.target.value)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel for="city">Registration</FormLabel>
@@ -125,6 +197,8 @@ const EditTentant = () => {
                   id="registrationofRoomRent"
                   type="date"
                   placeholder="Registration-of-date"
+                  value={registrationofRoomRent}
+                  onChange={(e) => setRegistrationofRoomRent(e.target.value)}
                 />
               </FormControl>
             </Flex>
@@ -132,11 +206,33 @@ const EditTentant = () => {
             <Flex>
               <FormControl>
                 <FormLabel for="street">street</FormLabel>
-                <Input id="Residentalstreet" type="text" placeholder="street" />
+                <Input
+                  id="Residentalstreet"
+                  type="text"
+                  placeholder="street"
+                  value={residentialAddress.street}
+                  onChange={(e) =>
+                    setResidentialAddress({
+                      ...residentialAddress,
+                      street: e.target.value,
+                    })
+                  }
+                />
               </FormControl>
               <FormControl>
                 <FormLabel for="city">city</FormLabel>
-                <Input id="Residentalcity" type="text" placeholder="city" />
+                <Input
+                  id="Residentalcity"
+                  type="text"
+                  placeholder="city"
+                  value={residentialAddress.city}
+                  onChange={(e) =>
+                    setResidentialAddress({
+                      ...residentialAddress,
+                      city: e.target.value,
+                    })
+                  }
+                />
               </FormControl>
             </Flex>
             <Flex>
@@ -147,6 +243,13 @@ const EditTentant = () => {
                   type="text"
                   placeholder="state"
                   ia-placeholder="state"
+                  value={residentialAddress.state}
+                  onChange={(e) =>
+                    setResidentialAddress({
+                      ...residentialAddress,
+                      state: e.target.value,
+                    })
+                  }
                 />
               </FormControl>
               <FormControl>
@@ -155,6 +258,13 @@ const EditTentant = () => {
                   id="Residentalcountry"
                   type="text"
                   placeholder="country"
+                  value={residentialAddress.country}
+                  onChange={(e) =>
+                    setResidentialAddress({
+                      ...residentialAddress,
+                      country: e.target.value,
+                    })
+                  }
                 />
               </FormControl>
               <FormControl>
@@ -163,31 +273,80 @@ const EditTentant = () => {
                   id="ResidentalzipCode"
                   type="text"
                   placeholder="postalCode"
+                  value={residentialAddress.zipCode}
+                  onChange={(e) =>
+                    setResidentialAddress({
+                      ...residentialAddress,
+                      zipCode: e.target.value,
+                    })
+                  }
                 />
               </FormControl>
             </Flex>
-            <Box class="form-check">
-              <Checkbox>Same Address</Checkbox>
-            </Box>
+            <Checkbox onChange={handleCheckbox}>Same Address</Checkbox>
             <p>currentAddress</p>
             <Flex>
               <FormControl>
                 <FormLabel for="street">street</FormLabel>
-                <Input id="Currentstreet" type="text" placeholder="street" />
+                <Input
+                  id="Currentstreet"
+                  type="text"
+                  placeholder="street"
+                  value={currentAddress.street}
+                  onChange={(e) =>
+                    setCurrentAddress({
+                      ...currentAddress,
+                      street: e.target.value,
+                    })
+                  }
+                />
               </FormControl>
               <FormControl>
                 <FormLabel for="city">city</FormLabel>
-                <Input id="Currentcity" type="text" placeholder="city" />
+                <Input
+                  id="Currentcity"
+                  type="text"
+                  placeholder="city"
+                  value={currentAddress.city}
+                  onChange={(e) =>
+                    setCurrentAddress({
+                      ...currentAddress,
+                      street: e.target.value,
+                    })
+                  }
+                />
               </FormControl>
             </Flex>
             <Flex>
               <FormControl>
                 <FormLabel for="state">state</FormLabel>
-                <Input id="Currentstate" type="text" placeholder="state" />
+                <Input
+                  id="Currentstate"
+                  type="text"
+                  placeholder="state"
+                  value={currentAddress.state}
+                  onChange={(e) =>
+                    setCurrentAddress({
+                      ...currentAddress,
+                      state: e.target.value,
+                    })
+                  }
+                />
               </FormControl>
               <FormControl>
                 <FormLabel for="country">country</FormLabel>
-                <Input id="Currentcountry" type="text" placeholder="country" />
+                <Input
+                  id="Currentcountry"
+                  type="text"
+                  placeholder="country"
+                  value={currentAddress.country}
+                  onChange={(e) =>
+                    setCurrentAddress({
+                      ...currentAddress,
+                      country: e.target.value,
+                    })
+                  }
+                />
               </FormControl>
               <FormControl>
                 <FormLabel for="postalCode">postalCode</FormLabel>
@@ -195,11 +354,19 @@ const EditTentant = () => {
                   id="CurrentzipCode"
                   type="text"
                   placeholder="postalCode"
+                  value={currentAddress.zipCode}
+                  onChange={(e) =>
+                    setCurrentAddress({
+                      ...currentAddress,
+                      zipCode: e.target.value,
+                    })
+                  }
                 />
               </FormControl>
             </Flex>
           </Box>
           <Button
+            type="submit"
             className="addPropertybutton"
             _hover={{ bg: "green", color: "white" }}
             style={{
@@ -208,7 +375,6 @@ const EditTentant = () => {
               margin: "auto",
               fontSize: "24px",
             }}
-            onClick={handleEditTentant}
           >
             update
           </Button>
